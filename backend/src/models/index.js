3dca9fcd2818007221ef20130bd88e6493a6ca01
yash70g <yash70g@gmail.com>
2026-02-22 14:06:35 +0530

    register device upon heartbeat in device table

--- backend/src/models/index.js
+++ backend/src/models/index.js
@@ -13,36 +13,42 @@ async function createHeartbeat(data) {
   return Heartbeat.create(data);
 }
 
-// Query devices by region and version
 async function getDevicesByRegionAndVersion(region, version) {
   const query = {};
   if (region) query.region = region;
   if (version) query.currentVersion = version;
-  
   return Device.find(query).lean();
 }
 
-// Get device count by criteria
 async function getDeviceCountByRegionAndVersion(region, version) {
   const query = {};
   if (region) query.region = region;
   if (version) query.currentVersion = version;
-  
   return Device.countDocuments(query);
 }
 
-// Get all devices with optional filters
 async function getAllDevices(filters = {}) {
   return Device.find(filters).lean();
 }
 
-// Update device last heartbeat
 async function updateDeviceHeartbeat(deviceId, timestamp = new Date()) {
   return Device.findOneAndUpdate(
     { deviceId },
     { lastHeartbeatAt: timestamp },
     { new: true }
   );
+}
+
+async function registerOrUpdateDevice(deviceId, data = {}) {
+  return Device.findOneAndUpdate(
+    { deviceId },
+    { ...data, lastHeartbeatAt: new Date() },
+    { upsert: true, new: true }
+  );
+}
+
+async function registerDevice(deviceData) {
+  return Device.create(deviceData);
 }
 
 module.exports = {
@@ -57,5 +63,7 @@ module.exports = {
   getDevicesByRegionAndVersion,
   getDeviceCountByRegionAndVersion,
   getAllDevices,
-  updateDeviceHeartbeat
+  updateDeviceHeartbeat,
+  registerOrUpdateDevice,
+  registerDevice
 };
