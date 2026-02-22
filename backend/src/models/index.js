a757ad8d0b2f88278d611951abe42559c061667a
yash70g <yash70g@gmail.com>
2026-02-22 13:42:05 +0530

    added and tested devices endpoint

--- backend/src/models/index.js
+++ backend/src/models/index.js
@@ -1,8 +1,7 @@
 const Device = require("./device.model");
-const AppVersion = require("./appVersion.model");
-const Rollout = require("./rollout.model");
-const VersionTransition = require("./versionTransition.model");
-const DeviceUpdate = require("./deviceUpdate.model");
+// const Rollout = require("./rollout.model");
+// const VersionTransition = require("./versionTransition.model");
+// const DeviceUpdate = require("./deviceUpdate.model");
 const AuditEvent = require("./auditEvent.model");
 const Heartbeat = require("./heartbeat.model");
 
@@ -14,14 +13,49 @@ async function createHeartbeat(data) {
   return Heartbeat.create(data);
 }
 
+// Query devices by region and version
+async function getDevicesByRegionAndVersion(region, version) {
+  const query = {};
+  if (region) query.region = region;
+  if (version) query.currentVersion = version;
+  
+  return Device.find(query).lean();
+}
+
+// Get device count by criteria
+async function getDeviceCountByRegionAndVersion(region, version) {
+  const query = {};
+  if (region) query.region = region;
+  if (version) query.currentVersion = version;
+  
+  return Device.countDocuments(query);
+}
+
+// Get all devices with optional filters
+async function getAllDevices(filters = {}) {
+  return Device.find(filters).lean();
+}
+
+// Update device last heartbeat
+async function updateDeviceHeartbeat(deviceId, timestamp = new Date()) {
+  return Device.findOneAndUpdate(
+    { deviceId },
+    { lastHeartbeatAt: timestamp },
+    { new: true }
+  );
+}
+
 module.exports = {
   Device,
-  AppVersion,
-  Rollout,
-  VersionTransition,
-  DeviceUpdate,
+  // Rollout,
+  // VersionTransition,
+  // DeviceUpdate,
   AuditEvent,
   Heartbeat,
-  createAuditEvent,
-  createHeartbeat
+  // createAuditEvent,
+  createHeartbeat,
+  getDevicesByRegionAndVersion,
+  getDeviceCountByRegionAndVersion,
+  getAllDevices,
+  updateDeviceHeartbeat
 };
