6ed537ab1ddbe0301ff93d01eddf4135a8296215
yash70g <yash70g@gmail.com>
2026-02-22 11:38:43 +0530

    schema update as per document

--- backend/src/models/device.model.js
+++ backend/src/models/device.model.js
@@ -2,15 +2,21 @@ const mongoose = require("mongoose");
 
 const DeviceSchema = new mongoose.Schema(
   {
-    deviceId: { type: String, required: true },
-    appId: { type: String, required: true },
-    platform: { type: String, required: true },
+    deviceId: { type: String, required: true }, //IMEI
+    platform: { type: String, required: true }, // e.g., device os
     currentVersion: { type: String, default: "0.0.0" },
     status: { type: String, default: "active" },
-    lastHeartbeatAt: { type: Date, default: null },
+    region: { type: String, default: "Bangalore" }, //location
+    lastHeartbeatAt: { type: Date, default: null }, //last app open time
     metadata: { type: mongoose.Schema.Types.Mixed, default: {} }
   },
   { timestamps: true, collection: "devices" }
 );
 
 module.exports = mongoose.model("Device", DeviceSchema);
+
+// IMEI Number (Primary Identifier)
+// ● App Version
+// ● Device OS & Model (optional but recommended)
+// ● Last App Open Time
+// ● Location/Region Tag
